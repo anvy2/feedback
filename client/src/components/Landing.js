bf9d2e472ba08740422fcb294a2a304728e12dbe
Anshul <anshul2679@gmail.com>
2020-06-20 11:25:36 +0530

    modified landing page to increase accessibility

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
   render() {
-    return <div>Landing</div>;
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h5>Welcome to demo Feedback App.</h5>
+
+        <Link to="/surveys" className="waves-effect waves-light btn-large">
+          Dashboard
+        </Link>
+      </div>
+    );
   }
 }
 
